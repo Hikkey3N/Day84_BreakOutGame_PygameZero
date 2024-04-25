dcfa7fca5040dd920b928e3120fc6d75bd4b8c03
HikkeyTheWeirdo <congnguyen051906@gmail.com>
2024-04-25 03:38:19 +0200

    Basically finished bar and implied it into the main screen, notthing else

--- bar.py
+++ bar.py
@@ -1,5 +1,33 @@
 import tkinter as tk
 
+WINDOW_WIDTH = 1200
+WINDOW_HEIGHT = 600
+
+BAR_WIDTH = 200
+BAR_HEIGHT = 20
+BAR_COLOR = '#89ABE3'
+
 class Bar:
     def __init__(self, master):
-        pass
+        self.master = master
+
+        self.x = (WINDOW_WIDTH/2) - (BAR_WIDTH/2)
+        self.y = WINDOW_HEIGHT * (5/6)
+
+        self.the_bar = tk.Frame(self.master, width=BAR_WIDTH, height=BAR_HEIGHT, bg=BAR_COLOR)
+        self.the_bar.place(x=self.x, y=self.y)
+
+
+    # Moving bar, if touches wall
+    def move_bar(self, event):
+        key = event.keysym
+
+        if key == "Left":
+            if self.x > 0:
+                self.x -= 20
+        
+        if key == "Right":
+            if self.x + BAR_WIDTH < 1200:
+                self.x += 20
+        
+        self.the_bar.place(x=self.x, y=self.y)
--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 import tkinter as tk
+from bar import Bar
 import time
+
+############################## GLOBAL VARIABLES ##############################
+BG_COLOR = "#EA738D"
+
+# Create the main window
+root = tk.Tk()
+bar = Bar(root)
+root.configure(bg=BG_COLOR)
+
+# Set the window size
+window_width = 1200
+window_height = 600
+root.geometry(f"{window_width}x{window_height}")
+
+# Bind key events to the move_bar function
+root.bind("<KeyPress>", bar.move_bar)
+
+# Start the Tkinter event loop
+root.mainloop()
